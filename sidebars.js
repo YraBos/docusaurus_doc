55a997a590c02a76ab190d0737e12df244147f7a
YraBos <yrabos64@gmail.com>
2024-07-04 00:56:33 +0300

    New

--- sidebars.js
+++ sidebars.js
@@ -7,7 +7,15 @@ module.exports = {
             label: 'Docusaurus',
             link: {type: 'doc', id: 'docusaurus/docusaurus'},
             items: [
-                'docusaurus/install','docusaurus/create',
+                'docusaurus/install', {
+                    type: 'category',
+                    label: 'Создание проекта',
+                    link: {type: 'doc', id: 'docusaurus/create'},
+                    items: [
+                        'docusaurus/create_local',
+                        'docusaurus/create_deploy',
+                    ],
+                }
            ],
         },
 
